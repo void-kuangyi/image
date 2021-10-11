b04e9563df6223acc8a1f393a8713dffce45f006
Kuangyi <kuanyi@Kuanyis-MacBook-Pro.local>
2021-10-11 22:26:50 +0200

    process all the greyscale value

--- processing.py
+++ processing.py
@@ -8,15 +8,11 @@ def function():
     img = Image.open(BytesIO(response.content)).convert('L')  
 
     # Resize smoothly down to 16x16 pixels
-    imgSmall = img.resize((5,5),resample=Image.BILINEAR)
-    # imgGrey = ImageOps.grayscale(imgSmall)
-
-    # Scale back up using NEAREST to original size
-    result = imgSmall.resize(img.size,Image.NEAREST)
-
-    rawData = result.load()
+    imgSmall = img.resize((4,4),resample=Image.BILINEAR)
+    imgGrey = ImageOps.grayscale(imgSmall)
+    rawData = imgGrey.load()
     data = []
-    for y in range(5):
-        for x in range(5):
+    for y in range(4):
+        for x in range(4):
             data.append(rawData[x,y])
-    return str(data[0])
+    return str(data)
