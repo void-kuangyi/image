9d3a43799ecf5dd57f44b7e4487a61bec5fd087e
Kuangyi <kuanyi@Kuanyis-MacBook-Pro.local>
2021-10-11 22:39:13 +0200

    update

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask import request
 from processing import function
 
 app = Flask(__name__)
-@app.route("/<str:variable>", methods=['GET'])
-def index(varible):
+@app.route("/<variable>", methods=['GET'])
+def index(variable):
     if request.method == 'GET':
-        return function(varible)
+        return function(variable)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))        
