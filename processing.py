ab4876a01ba4133a4afebf2934afb315e4892c11
Kuangyi <kuanyi@Kuanyis-MacBook-Pro.local>
2021-10-15 16:16:17 +0200

    accept the coordinates

--- processing.py
+++ processing.py
@@ -3,9 +3,9 @@ import requests
 from io import BytesIO
 
 def function(variable):
-    url = "https://maps.googleapis.com/maps/api/staticmap?center=51.450102,%205.489436&zoom=16&size=400x400&key=AIzaSyDLLb5FAlXngV3Q16xGZAkNKxwMCNMJBtc"
+    url = f'https://maps.googleapis.com/maps/api/staticmap?center={variable}&zoom=16&size=400x400&maptype=satellite&key=AIzaSyDLLb5FAlXngV3Q16xGZAkNKxwMCNMJBtc'
     response = requests.get(url)
-    img = Image.open(BytesIO(response.content)).convert('L')  
+    img = Image.open(BytesIO(response.content)).convert("L")
 
     # Resize smoothly down to 16x16 pixels
     imgSmall = img.resize((4,4),resample=Image.BILINEAR)
