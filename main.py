bccbbc2dd13f1bcd40465e4693799b79d831e0c6
Kuangyi <kuanyi@wlan-163010.nbw.tue.nl>
2021-10-15 11:13:07 +0200

    trigger a build

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route("/<variable>", methods=['GET'])
 def index(variable):
     if request.method == 'GET':
+        
         return function(variable)
 
 if __name__ == "__main__":
